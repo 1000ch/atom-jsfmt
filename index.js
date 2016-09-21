bd0cf62911d0ba40265404d16050635ff600628f
1000ch <shogo.sensui@gmail.com>
2016-09-22 01:07:18 +0900

    Use CompositeDisposable

--- index.js
+++ index.js
@@ -2,8 +2,35 @@
 
 import { normalize, join } from 'path';
 import { spawn } from 'child_process';
+import { CompositeDisposable } from 'atom';
 
 const JSFMT_PATH = normalize(join(__dirname, 'node_modules', '.bin', 'jsfmt'));
+
+let subscriptions;
+let editorObserver;
+let formatOnSave;
+
+export function activate(state) {
+  subscriptions = new CompositeDisposable();
+  subscriptions.add(atom.config.observe('jsfmt.formatOnSave', value => formatOnSave = value));
+
+  atom.commands.add('atom-workspace', 'jsfmt:execute', () => {
+    execute();
+  });
+
+  editorObserver = atom.workspace.observeTextEditors(editor => {
+    editor.getBuffer().onWillSave(() => {
+      if (formatOnSave) {
+        execute();
+      }
+    });
+  });
+}
+
+export function deactivate() {
+  subscriptions.dispose();
+  editorObserver.dispose();
+}
 
 function execute() {
   const editor = atom.workspace.getActiveTextEditor();
@@ -36,31 +63,4 @@ function execute() {
     editor.setText(Buffer.concat(chunks).toString());
     editor.setCursorBufferPosition(position);
   });
-}
-
-let editorObserver;
-let formatOnSave;
-
-export function activate(state) {
-  atom.commands.add('atom-workspace', 'jsfmt:execute', () => {
-    execute();
-  });
-
-  editorObserver = atom.workspace.observeTextEditors(editor => {
-    editor.getBuffer().onWillSave(() => {
-      if (formatOnSave) {
-        execute();
-      }
-    });
-  });
-
-  formatOnSave = atom.config.get('jsfmt.formatOnSave');
-
-  atom.config.observe('jsfmt.formatOnSave', value => {
-    formatOnSave = value;
-  });
-}
-
-export function deactivate() {
-  editorObserver.dispose();
 }
